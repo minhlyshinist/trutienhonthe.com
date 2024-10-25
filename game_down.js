ad227a039bdbd20a0cc72a3374123366f23c5baf
minhlyshinist <minhly.shinist@gmail.com>
2024-10-25 22:54:28 +0700

    dsfsdf

---
+++ game_down.js
@@ -0,0 +1,11 @@
+$('.btn_ios').attr('href', sea_download_json.ios);
+$('.btn_goo').attr('href', sea_download_json.googleplay);
+$('.btn_hw').attr('href', sea_download_json.customChanel1);
+$('.btn_pc').attr('href', sea_download_json.desktop);
+$('.down_qrcode').attr('src', sea_download_json.qrcode);
+
+
+//meta分享
+$('#meta_title').attr('content', document.title);
+$('#meta_name').attr('content', document.title);
+$('#meta_url').attr('content', window.location.href);
